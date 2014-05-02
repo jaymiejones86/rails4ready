90562edc1f17ad1d88ea34a121e12a28a14c8dce
Jaymie Jones <jaymiejones86@gmail.com>
2014-05-02 20:35:05 +1000

    Completed Postgres Setup

--- roles/database.rb
+++ roles/database.rb
@@ -2,6 +2,7 @@ name "database"
 
 run_list(
   "recipe[postgresql::server]",
+  "recipe[postgresql::ruby]",
   "recipe[rails4box::postgres]"
 )
 
@@ -16,9 +17,7 @@ default_attributes(
     'config' => {
       'listen_addresses' => 'localhost',
       'port' => 5432,
-      'ssl' => true,
-      'ssl_cert_file' => "./site-cookbooks/rails4box/files/default/pg.pem",
-      'ssl_key_file' => "./site-cookbooks/rails4box/files/default/pg.key"
+      'ssl' => false
     },
     'password' => {
       'postgres' => 'vagrant'
@@ -27,3 +26,26 @@ default_attributes(
     'enable_pgdg_apt' => true
   }
 )
+
+# default_attributes(
+#   'pg' => {
+#     'db_name' => "vagrantapp",
+#     'username' => 'vagrantapp',
+#     'host' => 'localhost',
+#     'password' => 'vagrant'
+#   },
+#   'postgresql' => {
+#     'config' => {
+#       'listen_addresses' => 'localhost',
+#       'port' => 5432,
+#       'ssl' => false,
+#       'ssl_cert_file' => "./site-cookbooks/rails4box/files/default/pg.pem",
+#       'ssl_key_file' => "./site-cookbooks/rails4box/files/default/pg.key"
+#     },
+#     'password' => {
+#       'postgres' => 'vagrant'
+#     },
+#     'version' => "9.3",
+#     'enable_pgdg_apt' => true
+#   }
+# )
--- site-cookbooks/rails4box/recipes/postgres.rb
+++ site-cookbooks/rails4box/recipes/postgres.rb
@@ -7,31 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# include_recipe "database::postgresql"
+include_recipe "database::postgresql"
+include_recipe "postgresql::server"
+include_recipe "postgresql::ruby"
 
-# postgresql_database "#{node['pg']['db_name']}" do
-#   connection(
-#     :host => node['pg']['host'],
-#     :port => 5432,
-#     :username => 'postgres',
-#     :password => node['pg']['password']
-#   )
-#   action :create
-# end
+postgresql_database "#{node['pg']['db_name']}" do
+  connection(
+    :host => node['pg']['host'],
+    :port => 5432,
+    :username => 'postgres',
+    :password => node['pg']['password']
+  )
+  action :create
+end
 
-# postgresql_connection_info = {
-#   :host     => node['pg']['host'],
-#   :port     => 5432,
-#   :username => "postgres",
-#   :password => node['pg']['password']
-# }
+postgresql_connection_info = {
+  :host     => node['pg']['host'],
+  :port     => 5432,
+  :username => "postgres",
+  :password => node['pg']['password']
+}
 
-# database_user node['pg']['username'] do
-#   connection postgresql_connection_info
-#   password   node['pg']['password']
-#   provider   Chef::Provider::Database::PostgresqlUser
-#   action     :create
-# end
+database_user node['pg']['username'] do
+  connection postgresql_connection_info
+  password   node['pg']['password']
+  provider   Chef::Provider::Database::PostgresqlUser
+  action     :create
+end
 
 # service "postgresql" do
 #   service_name "postgresql"
